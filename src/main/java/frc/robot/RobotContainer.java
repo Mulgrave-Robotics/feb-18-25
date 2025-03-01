05cd9d44f63b73b4d0bbbbb84a3a5b385a43a4a9
IgnacioR97 <ignaciorevello21@gmail.com>
2025-02-28 22:57:35 -0800

    added field centric and acceleration

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,23 +1,12 @@
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.ButtonConstants;
-import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.ElevatorConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.CoralIntakeSubsystem;
@@ -25,71 +14,50 @@ import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
 
 public class RobotContainer {
-        private final DriveSubsystem m_robotDrive = new DriveSubsystem();
-        private final ElevatorSubsystem m_elevator = new ElevatorSubsystem();
-        private final CommandXboxController m_driverController = new CommandXboxController(
-                        OIConstants.kDriverControllerPort);
-        private final CoralIntakeSubsystem coralIntake = new CoralIntakeSubsystem();
+    private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+    private final ElevatorSubsystem m_elevator = new ElevatorSubsystem();
+    private final CommandXboxController m_driverController = new CommandXboxController(OIConstants.kDriverControllerPort);
+    private final CoralIntakeSubsystem coralIntake = new CoralIntakeSubsystem();
 
-        private final double speedMultiplier = 0.5;
+    private final double speedMultiplier = 0.7;
 
-        public RobotContainer() {
-                configureButtonBindings();
+    public RobotContainer() {
+        configureButtonBindings();
 
-                // Default driving control using Xbox controller
-                m_robotDrive.setDefaultCommand(
-                                new RunCommand(
-                                                () -> m_robotDrive.drive(
-                                                                -MathUtil.applyDeadband(m_driverController.getLeftY(),
-                                                                                OIConstants.kDriveDeadband)
-                                                                                * speedMultiplier, // Y-axis:
-                                                                // forward/backward
-                                                                -MathUtil.applyDeadband(m_driverController.getLeftX(),
-                                                                                OIConstants.kDriveDeadband)
-                                                                                * speedMultiplier, // X-axis:
-                                                                // left/right
-                                                                -MathUtil.applyDeadband(m_driverController.getRightX(),
-                                                                                OIConstants.kDriveDeadband)
-                                                                                * speedMultiplier, // Right
-                                                                // thumbstick:
-                                                                // rotation
-                                                                true),
-                                                m_robotDrive));
-        }
+        // Default driving control using Xbox controller
+        m_robotDrive.setDefaultCommand(
+            new RunCommand(
+                () -> m_robotDrive.drive(
+                    -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband) * speedMultiplier, // Y-axis: forward/backward
+                    -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband) * speedMultiplier, // X-axis: left/right
+                    -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband) * speedMultiplier, // Right stick: rotation
+                    true),
+                m_robotDrive
+            )
+        );
+    }
 
-        private void configureButtonBindings() {
-                // Map Xbox controller buttons to elevator commands
-                // m_driverController.button(ButtonConstants.xboxY).onTrue(m_elevator.goToLevel(1));
-                // // Y Button
-                // m_driverController.button(Button.kB.value).onTrue(m_elevator.goToLevel(2));
-                // // B Button
-                // m_driverController.button(Button.kX.value).onTrue(m_elevator.goToLevel(0));
-                // // X Button (Reset)
+    private void configureButtonBindings() {
+        // Elevator Controls
+        m_driverController.button(ButtonConstants.xboxY).onTrue(m_elevator.moveTo(ElevatorConstants.vL3Height));
+        m_driverController.button(ButtonConstants.xboxB).onTrue(m_elevator.moveTo(ElevatorConstants.vL2Height));
+        m_driverController.button(ButtonConstants.xboxA).onTrue(m_elevator.moveTo(ElevatorConstants.vL1Height));
 
-                m_driverController.button(ButtonConstants.xboxY).onTrue(m_elevator.moveTo(ElevatorConstants.vL3Height));
-                // m_driverController.button(ButtonConstants.xboxX).onTrue(m_elevator.moveTo(ElevatorConstants.vL3Height));
-                m_driverController.button(ButtonConstants.xboxB).onTrue(m_elevator.moveTo(ElevatorConstants.vL2Height));
-                m_driverController.button(ButtonConstants.xboxA).onTrue(m_elevator.moveTo(ElevatorConstants.vL1Height));
+        // Coral Intake Controls
+        m_driverController.button(ButtonConstants.xboxRB)
+            .onTrue(coralIntake.setCoralIntakeRoller(Constants.IntakeConstants.CoralIntakeSpeeds));
+        m_driverController.button(ButtonConstants.xboxLB)
+            .onTrue(coralIntake.setCoralIntakeRoller(Constants.IntakeConstants.CoralOuttakeSpeeds));
+        m_driverController.button(ButtonConstants.xboxX)
+            .onTrue(coralIntake.setCoralIntakeRoller(0));
+    }
 
-                // Example: Adding button for autonomous/other control (if needed)
-                m_driverController.button(ButtonConstants.xboxRB)
-                                .onTrue(coralIntake.setCoralIntakeRoller(Constants.IntakeConstants.CoralIntakeSpeeds));
-
-                m_driverController.button(ButtonConstants.xboxLB)
-                                .onTrue(coralIntake.setCoralIntakeRoller(Constants.IntakeConstants.CoralOuttakeSpeeds));
-
-                m_driverController.button(ButtonConstants.xboxX)
-                                .onTrue(coralIntake.setCoralIntakeRoller(0));
-
-                // For example, RB and LB could be mapped to different drive modes or additional
-                // control commands.
-        }
-
-        // Autonomous command: Move forward for 3 seconds
-        public Command getAutonomousCommand() {
-                return new SequentialCommandGroup(
-                                new RunCommand(() -> m_robotDrive.drive(0.2, 0, 0, false), m_robotDrive)
-                                                .withTimeout(3),
-                                new InstantCommand(() -> m_robotDrive.drive(0, 0, 0, false)));
-        }
+    // Autonomous command: Move forward for 3 seconds
+    public Command getAutonomousCommand() {
+        return new SequentialCommandGroup(
+            new RunCommand(() -> m_robotDrive.drive(0.2, 0, 0, false), m_robotDrive)
+                .withTimeout(3),
+            new InstantCommand(() -> m_robotDrive.drive(0, 0, 0, false)) // Stop movement
+        );
+    }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.hal.FRCNetComm.tInstances;
@@ -13,58 +9,39 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.ADIS16470_IMU;
 import edu.wpi.first.wpilibj.ADIS16470_IMU.IMUAxis;
 import frc.robot.Constants.DriveConstants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
-  // Create MAXSwerveModules
-  private final MAXSwerveModule m_frontLeft = new MAXSwerveModule(
-      DriveConstants.kFrontLeftDrivingCanId,
-      DriveConstants.kFrontLeftTurningCanId,
-      DriveConstants.kFrontLeftChassisAngularOffset);
+    private final MAXSwerveModule m_frontLeft = new MAXSwerveModule(
+        DriveConstants.kFrontLeftDrivingCanId,
+        DriveConstants.kFrontLeftTurningCanId,
+        DriveConstants.kFrontLeftChassisAngularOffset);
+    private final MAXSwerveModule m_frontRight = new MAXSwerveModule(
+        DriveConstants.kFrontRightDrivingCanId,
+        DriveConstants.kFrontRightTurningCanId,
+        DriveConstants.kFrontRightChassisAngularOffset);
+    private final MAXSwerveModule m_rearLeft = new MAXSwerveModule(
+        DriveConstants.kRearLeftDrivingCanId,
+        DriveConstants.kRearLeftTurningCanId,
+        DriveConstants.kBackLeftChassisAngularOffset);
+    private final MAXSwerveModule m_rearRight = new MAXSwerveModule(
+        DriveConstants.kRearRightDrivingCanId,
+        DriveConstants.kRearRightTurningCanId,
+        DriveConstants.kBackRightChassisAngularOffset);
 
-  private final MAXSwerveModule m_frontRight = new MAXSwerveModule(
-      DriveConstants.kFrontRightDrivingCanId,
-      DriveConstants.kFrontRightTurningCanId,
-      DriveConstants.kFrontRightChassisAngularOffset);
+    private final ADIS16470_IMU m_gyro = new ADIS16470_IMU();
 
-  private final MAXSwerveModule m_rearLeft = new MAXSwerveModule(
-      DriveConstants.kRearLeftDrivingCanId,
-      DriveConstants.kRearLeftTurningCanId,
-      DriveConstants.kBackLeftChassisAngularOffset);
+    // ✅ Acceleration Control Variables
+    private double prevXSpeed = 0;
+    private double prevYSpeed = 0;
+    private double prevRotSpeed = 0;
+    private static final double MAX_ACCELERATION = 0.1; // Limits acceleration increase per loop
 
-  private final MAXSwerveModule m_rearRight = new MAXSwerveModule(
-      DriveConstants.kRearRightDrivingCanId,
-      DriveConstants.kRearRightTurningCanId,
-      DriveConstants.kBackRightChassisAngularOffset);
-
-  // The gyro sensor
-  private final ADIS16470_IMU m_gyro = new ADIS16470_IMU();
-
-  // Odometry class for tracking robot pose
-  SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(
-      DriveConstants.kDriveKinematics,
-      Rotation2d.fromDegrees(m_gyro.getAngle(IMUAxis.kZ)),
-      new SwerveModulePosition[] {
-          m_frontLeft.getPosition(),
-          m_frontRight.getPosition(),
-          m_rearLeft.getPosition(),
-          m_rearRight.getPosition()
-      });
-
-  /** Creates a new DriveSubsystem. */
-  public DriveSubsystem() {
-    // Usage reporting for MAXSwerve template
-    HAL.report(tResourceType.kResourceType_RobotDrive, tInstances.kRobotDriveSwerve_MaxSwerve);
-  }
-
-  @Override
-  public void periodic() {
-    // Update the odometry in the periodic block
-    m_odometry.update(
+    SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(
+        DriveConstants.kDriveKinematics,
         Rotation2d.fromDegrees(m_gyro.getAngle(IMUAxis.kZ)),
         new SwerveModulePosition[] {
             m_frontLeft.getPosition(),
@@ -72,114 +49,83 @@ public class DriveSubsystem extends SubsystemBase {
             m_rearLeft.getPosition(),
             m_rearRight.getPosition()
         });
-  }
 
-  /**
-   * Returns the currently-estimated pose of the robot.
-   *
-   * @return The pose.
-   */
-  public Pose2d getPose() {
-    return m_odometry.getPoseMeters();
-  }
+    public DriveSubsystem() {
+        HAL.report(tResourceType.kResourceType_RobotDrive, tInstances.kRobotDriveSwerve_MaxSwerve);
+    }
 
-  /**
-   * Resets the odometry to the specified pose.
-   *
-   * @param pose The pose to which to set the odometry.
-   */
-  public void resetOdometry(Pose2d pose) {
-    m_odometry.resetPosition(
-        Rotation2d.fromDegrees(m_gyro.getAngle(IMUAxis.kZ)),
-        new SwerveModulePosition[] {
-            m_frontLeft.getPosition(),
-            m_frontRight.getPosition(),
-            m_rearLeft.getPosition(),
-            m_rearRight.getPosition()
-        },
-        pose);
-  }
+    @Override
+    public void periodic() {
+        m_odometry.update(
+            Rotation2d.fromDegrees(m_gyro.getAngle(IMUAxis.kZ)),
+            new SwerveModulePosition[] {
+                m_frontLeft.getPosition(),
+                m_frontRight.getPosition(),
+                m_rearLeft.getPosition(),
+                m_rearRight.getPosition()
+            });
+    }
 
-  /**
-   * Method to drive the robot using joystick info.
-   *
-   * @param xSpeed        Speed of the robot in the x direction (forward).
-   * @param ySpeed        Speed of the robot in the y direction (sideways).
-   * @param rot           Angular rate of the robot.
-   * @param fieldRelative Whether the provided x and y speeds are relative to the
-   *                      field.
-   */
-  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
-    // Convert the commanded speeds into the correct units for the drivetrain
-    double xSpeedDelivered = xSpeed * DriveConstants.kMaxSpeedMetersPerSecond;
-    double ySpeedDelivered = ySpeed * DriveConstants.kMaxSpeedMetersPerSecond;
-    double rotDelivered = rot * DriveConstants.kMaxAngularSpeed;
+    public Pose2d getPose() {
+        return m_odometry.getPoseMeters();
+    }
 
-    var swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
-        fieldRelative
-            ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered,
-                Rotation2d.fromDegrees(m_gyro.getAngle(IMUAxis.kZ)))
-            : new ChassisSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered));
-    SwerveDriveKinematics.desaturateWheelSpeeds(
-        swerveModuleStates, DriveConstants.kMaxSpeedMetersPerSecond);
-    m_frontLeft.setDesiredState(swerveModuleStates[0]);
-    m_frontRight.setDesiredState(swerveModuleStates[1]);
-    m_rearLeft.setDesiredState(swerveModuleStates[2]);
-    m_rearRight.setDesiredState(swerveModuleStates[3]);
-  }
+    public void resetOdometry(Pose2d pose) {
+        m_odometry.resetPosition(
+            Rotation2d.fromDegrees(m_gyro.getAngle(IMUAxis.kZ)),
+            new SwerveModulePosition[] {
+                m_frontLeft.getPosition(),
+                m_frontRight.getPosition(),
+                m_rearLeft.getPosition(),
+                m_rearRight.getPosition()
+            },
+            pose);
+    }
 
-  /**
-   * Sets the wheels into an X formation to prevent movement.
-   */
-  public void setX() {
-    m_frontLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
-    m_frontRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
-    m_rearLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
-    m_rearRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
-  }
+    public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
+        // ✅ Apply Acceleration Smoothing
+        xSpeed = applyAccelerationRamp(prevXSpeed, xSpeed);
+        ySpeed = applyAccelerationRamp(prevYSpeed, ySpeed);
+        rot = applyAccelerationRamp(prevRotSpeed, rot);
 
-  /**
-   * Sets the swerve ModuleStates.
-   *
-   * @param desiredStates The desired SwerveModule states.
-   */
-  public void setModuleStates(SwerveModuleState[] desiredStates) {
-    SwerveDriveKinematics.desaturateWheelSpeeds(
-        desiredStates, DriveConstants.kMaxSpeedMetersPerSecond);
-    m_frontLeft.setDesiredState(desiredStates[0]);
-    m_frontRight.setDesiredState(desiredStates[1]);
-    m_rearLeft.setDesiredState(desiredStates[2]);
-    m_rearRight.setDesiredState(desiredStates[3]);
-  }
+        prevXSpeed = xSpeed;
+        prevYSpeed = ySpeed;
+        prevRotSpeed = rot;
 
-  /** Resets the drive encoders to currently read a position of 0. */
-  public void resetEncoders() {
-    m_frontLeft.resetEncoders();
-    m_rearLeft.resetEncoders();
-    m_frontRight.resetEncoders();
-    m_rearRight.resetEncoders();
-  }
+        double xSpeedDelivered = xSpeed * DriveConstants.kMaxSpeedMetersPerSecond;
+        double ySpeedDelivered = ySpeed * DriveConstants.kMaxSpeedMetersPerSecond;
+        double rotDelivered = rot * DriveConstants.kMaxAngularSpeed;
 
-  /** Zeroes the heading of the robot. */
-  public void zeroHeading() {
-    m_gyro.reset();
-  }
+        // WITHOUT FIELD ORITENTATED DRIVE
+        // var swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
+        //     fieldRelative
+        //         ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered,
+        //         Rotation2d.fromDegrees(m_gyro.getAngle(IMUAxis.kZ)))
+        //         : new ChassisSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered));
 
-  /**
-   * Returns the heading of the robot.
-   *
-   * @return the robot's heading in degrees, from -180 to 180
-   */
-  public double getHeading() {
-    return Rotation2d.fromDegrees(m_gyro.getAngle(IMUAxis.kZ)).getDegrees();
-  }
+        // FIELD ORITNETATED DRIVE
+        var chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
+        xSpeedDelivered, 
+        ySpeedDelivered, 
+        rotDelivered,
+        Rotation2d.fromDegrees(-m_gyro.getAngle(IMUAxis.kZ)) // Use negative to correct for gyro rotation
+        );
+        var swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
+        // END
 
-  /**
-   * Returns the turn rate of the robot.
-   *
-   * @return The turn rate of the robot, in degrees per second
-   */
-  public double getTurnRate() {
-    return m_gyro.getRate(IMUAxis.kZ) * (DriveConstants.kGyroReversed ? -1.0 : 1.0);
-  }
+
+        SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, DriveConstants.kMaxSpeedMetersPerSecond);
+
+        m_frontLeft.setDesiredState(swerveModuleStates[0]);
+        m_frontRight.setDesiredState(swerveModuleStates[1]);
+        m_rearLeft.setDesiredState(swerveModuleStates[2]);
+        m_rearRight.setDesiredState(swerveModuleStates[3]);
+    }
+
+    private double applyAccelerationRamp(double previousSpeed, double desiredSpeed) {
+        double speedDifference = desiredSpeed - previousSpeed;
+        return Math.abs(speedDifference) > MAX_ACCELERATION
+            ? previousSpeed + Math.signum(speedDifference) * MAX_ACCELERATION
+            : desiredSpeed;
+    }
 }
